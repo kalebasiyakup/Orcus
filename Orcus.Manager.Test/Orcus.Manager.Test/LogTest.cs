5116e4f1fd391146d69044e5f51e2464d540c737
YaMi <kalebasiyakup@gmail.com>
2016-10-17 22:38:36 +0300

    ..

---
+++ Orcus.Manager.Test/Orcus.Manager.Test/LogTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orcus.Log;
+using System;
+
+namespace Orcus.Manager.Test
+{
+    [TestClass]
+    public class LogTest
+    {
+        [TestMethod]
+        public void ConfigReaderlarDoğruCalisiyorMu()
+        {
+            TestConfigReader reader = new TestConfigReader("Orcus.Log,Orcus.Log.TraceLogger", "Orcus.Log,Orcus.Log.HtmlLogFormatter");
+            string logSourceValue = reader.ReadKey("LogSource");
+            string logFormatterValue = reader.ReadKey("LogFormatter");
+
+            Assert.AreEqual(logSourceValue, "Orcus.Log,Orcus.Log.TraceLogger");
+            Assert.AreEqual(logFormatterValue, "Orcus.Log,Orcus.Log.HtmlLogFormatter");
+        }
+
+        [TestMethod]
+        public void LoglamaDogruCalisiyorMu()
+        {
+            LogManager manager = LogManager.CreateInstance(new TestConfigReader("Orcus.Log,Orcus.Log.TraceLogger", "Orcus.Log,Orcus.Log.HtmlLogFormatter"));
+            Log.Log myLog = new Log.Log("Deneme mesaj", LogPriority.Normal, DateTime.Now);
+            bool result = manager.WriteLog(myLog);
+            Assert.AreEqual(result, true);
+        }
+    }
+}
---
+++ Orcus.Manager/Orcus.Manager/LogManagers/SmartLogManager.cs
@@ -0,0 +1,20 @@
+﻿using Orcus.Log;
+
+namespace Orcus.Manager
+{
+    public class SmartLogManager : LogManager
+    {
+        public SmartLogManager(IConfigReader configReader) : base(configReader)
+        {
+        }
+
+        public override bool WriteLog(ILog log)
+        {
+            if (log.Priority == LogPriority.RedAlert)
+            {
+                log.LogMessage = "-------- Yandım yandımöööööööööööööö." + log.LogMessage;
+            }
+            return base.WriteLog(log);
+        }
+    }
+}
